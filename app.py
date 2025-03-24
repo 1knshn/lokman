fb403a8436dc721330b0f9ad7608fd3460f458bc
1knshn <birkansahin10@gmail.com>
2025-03-24 22:32:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyB3PrvUYsD_3nbmsSr8cb4s3Vm5oqGJd7k")
 GEMINI_API_URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key={GEMINI_API_KEY}"
 
-genai.configure(api_key=os.environ["AIzaSyB3PrvUYsD_3nbmsSr8cb4s3Vm5oqGJd7k"])
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 generation_config = {
     "temperature": 1,
